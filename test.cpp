854dcb29608abeda4f697eb29d9dd070c6b5f075
windows <2465231972@qq.com>
2023-06-07 21:09:27 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,10 @@
+/*test.cpp*/
+#include <stdio.h>
+int main()
+{
+	printf("hello C++ in Linux!\n");
+	printf("hello C-- in Linux!\n");
+	printf("hello C// in Linux!\n");
+	printf("hello Cxx in Linux!\n");
+	return 0;
+}
